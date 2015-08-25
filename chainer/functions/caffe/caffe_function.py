9aeb5469600f85b72276338db566f14a4f6ed17d
Masaki Saito <msaito@vision.is.tohoku.ac.jp>
2015-08-25 19:02:29 +0900

    Fix typo

--- chainer/functions/caffe/caffe_function.py
+++ chainer/functions/caffe/caffe_function.py
@@ -385,7 +385,7 @@ def _get_height(blob):
         return blob.shape.dim[2]
     else:
         raise RuntimeError(
-            '{}-dimentional array is not supported'.format(
+            '{}-dimensional array is not supported'.format(
                 len(blob.shape.dim)))
 
 
@@ -398,5 +398,5 @@ def _get_width(blob):
         return blob.shape.dim[3]
     else:
         raise RuntimeError(
-            '{}-dimentional array is not supported'.format(
+            '{}-dimensional array is not supported'.format(
                 len(blob.shape.dim)))
